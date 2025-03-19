faa43d0776741a65e923931a992980a570abac9f
Matheus <crispertax27297@gmail.com>
2025-03-19 15:39:12 -0400

    Atualizando as versoes do parcel e sharp

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const dataDoEvento = new Date("Mar 20, 2025 19:00:00");
+const dataDoEvento = new Date("Mar 25, 2025 19:00:00");
 const timeStampDoEvento =dataDoEvento.getTime();
 
 const contaAsHoras = setInterval(function() {
